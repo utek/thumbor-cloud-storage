10f3bf10048dd6294df1ff0ea24ef8cc26efd933
Bradley Whittington <radbrad182@gmail.com>
2015-09-04 16:34:09 +0200

    Fix gcloud client

--- thumbor_cloud_storage/loaders/cloud_storage_loader.py
+++ thumbor_cloud_storage/loaders/cloud_storage_loader.py
@@ -1,10 +1,11 @@
-import gcloud.storage
+from tornado.concurrent import return_future
+from gcloud import storage
 
 def load(context, path, callback):
     bucket_id  = context.config.get("CLOUD_STORAGE_BUCKET_ID")
     project_id = context.config.get("CLOUD_STORAGE_PROJECT_ID")
-
-    bucket = gcloud.storage.get_bucket(bucket_id, project_id)
+    client = storage.Client(project_id)
+    bucket = client.get_bucket(bucket_id, project_id)
     blob = bucket.get_blob(path)
 
     if blob is None:
