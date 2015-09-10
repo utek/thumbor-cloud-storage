ef343204e2d413df9efc3cbdfd175e99fe550c16
Bradley Whittington <radbrad182@gmail.com>
2015-09-10 11:26:18 +0200

    Caches bucket object across requests

--- thumbor_cloud_storage/loaders/cloud_storage_loader.py
+++ thumbor_cloud_storage/loaders/cloud_storage_loader.py
@@ -1,12 +1,19 @@
 from tornado.concurrent import return_future
 from gcloud import storage
+from collections import defaultdict
+
+buckets = defaultdict(dict)
 
 @return_future
 def load(context, path, callback):
     bucket_id  = context.config.get("CLOUD_STORAGE_BUCKET_ID")
     project_id = context.config.get("CLOUD_STORAGE_PROJECT_ID")
-    client = storage.Client(project_id)
-    bucket = client.get_bucket(bucket_id)
+    bucket = buckets[project_id].get(bucket_id, None)
+    if bucket is None:
+        client = storage.Client(project_id)
+        bucket = client.get_bucket(bucket_id)
+        buckets[project_id][bucket_id] = bucket
+
     try:
         blob = bucket.get_blob(path)
     except:
