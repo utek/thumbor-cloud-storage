6a6ba750fcb60a45bae8254171a1bd60eb64b86a
William Stewart <zoidbergwill@gmail.com>
2017-04-18 14:04:50 +0200

    Bump thumbor + gcloud SDK, and lock on minimum versions

--- setup.py
+++ setup.py
@@ -2,9 +2,8 @@ from setuptools import setup
 from setuptools import find_packages
 
 REQUIREMENTS = [
-  "thumbor==5.0.3",
-  "gcloud==0.13.0",
-  "protorpc==0.10.0",
+  "thumbor>=6.3.1",
+  "gcloud>=0.18.3",
 ]
 
 setup(
